36b18f7a8f2673a537f6b35c494ef75e7cdf72bf
Aleksi Kervinen <me@aleksi.me>
2019-06-17 13:32:31 +0300

    add shore collision to boat

--- Assets/Scripts/BoatMovement.cs
+++ Assets/Scripts/BoatMovement.cs
@@ -9,25 +9,34 @@ public class BoatMovement : MonoBehaviour
     private PlayerInput input;
 
     private float startingPos;
+    private float leftBound;
+    private float rightBound;
 
     void Start()
     {
         input = GetComponent<PlayerInput>();
 
         startingPos = transform.position.z;
+
+        var river = GameObject.FindGameObjectWithTag("River").GetComponent<Renderer>();
+        var boatHalfWidth = transform.localScale.x / 2;
+
+        leftBound = river.bounds.min.x + boatHalfWidth;
+        rightBound = river.bounds.max.x - boatHalfWidth;
     }
-    
+
     void FixedUpdate()
     {
         var newPos = transform.position;
         newPos.z += boatSpeed * Time.deltaTime;
 
-       if (input.Pause)
+        if (input.Pause)
         {
             newPos.z = startingPos;
         }
 
         newPos.x += input.Horizontal * boatStrafeSpeed * Time.deltaTime;
+        newPos.x = Mathf.Clamp(newPos.x, leftBound, rightBound);
 
         transform.position = newPos;
     }
