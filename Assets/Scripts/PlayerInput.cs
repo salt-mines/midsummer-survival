1554f8e63165bbdd3cf6203fdfb80483e3e1eac4
Aleksi Kervinen <me@aleksi.me>
2019-06-17 11:21:50 +0300

    add basic boat movement

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public float Horizontal { get; private set; }
+    public bool Pause { get; private set; }
+    
+    void Update()
+    {
+        Horizontal = Input.GetAxisRaw("Horizontal");
+        Pause = Input.GetAxisRaw("Pause") > 0;
+    }
+}
