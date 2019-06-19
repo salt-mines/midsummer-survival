42c95975025fd2301f30676cfc220c34bb058232
Miika Minkkinen <c8mminkk@wpk.tpu.fi>
2019-06-19 10:58:41 +0300

    Level design started

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -78,7 +78,7 @@ public class GameManager : MonoBehaviour
         playerTimeWaited = 0;
         playerWaiting = true;
         player.GetComponent<BoatMovement>().isPaused = true;
-        var drunk = currentLevel * 1 / (levelList.Length - 1);
+        var drunk = currentLevel * 1f / (levelList.Length - 1);
         player.GetComponent<BoatMovement>().drunkLevel = drunk;
 
         if (drunkText)
